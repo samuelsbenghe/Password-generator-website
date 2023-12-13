cd6983dd998649afd99bc1d221eb29c962d1af57
Natalie451 <81078733+Natalie451@users.noreply.github.com>
2023-12-13 22:32:56 +0000

    Changed the colour and design of the password generator.
    Co-authored-by: Edith-Samuel Sbenghe <samuelsbenghe@users.noreply.github.com>

--- apps/password_generator.py
+++ apps/password_generator.py
@@ -10,11 +10,11 @@ def generate_password(number_of_characters: int, is_memorable:bool, add_numbers:
 
 # Generate a random password
 def generate_random_password(number_of_characters: int, add_numbers: bool, add_symbols: bool):
-    characters = list(string.ascii_letters) # ['a', 'b', 'c', ..., 'A', 'B', 'C', ...]
+    characters = list_of_characters() # ['a', 'b', 'c', ..., 'A', 'B', 'C', ...]
     if add_numbers:
-        characters += list(string.digits) # ['a', 'b', 'c', ..., 'A', 'B', 'C', ..., '0', '1', '2', ...]
+        characters += list_of_numbers() # ['a', 'b', 'c', ..., 'A', 'B', 'C', ..., '0', '1', '2', ...]
     if add_symbols:
-        characters += list(string.punctuation) # ['a', 'b', 'c', ..., 'A', 'B', 'C', ..., '0', '1', '2', ..., '!', '@', '#', ...]
+        characters += list_of_symbols() # ['a', 'b', 'c', ..., 'A', 'B', 'C', ..., '0', '1', '2', ..., '!', '@', '#', ...]
         
     password = ''
     if number_of_characters > 0:
@@ -26,12 +26,35 @@ def generate_random_password(number_of_characters: int, add_numbers: bool, add_s
 
 # Generate a memorable password
 def generate_memorable_password(number_of_characters: int, add_numbers: bool, add_symbols: bool):
-    words = return_all_words()
+    password_parts = list_of_words() # ['apple', 'banana', 'cherry', ...]
+    if add_numbers:
+        password_parts += list_of_numbers() # ['a', 'b', 'c', ..., 'A', 'B', 'C', ..., '0', '1', '2', ...]
+    if add_symbols:
+        password_parts += list_of_symbols() # ['a', 'b', 'c', ..., 'A', 'B', 'C', ..., '0', '1', '2', ..., '!', '@', '#', ...]
     password = ''
+
+    # if length of password is less than needed amount of characters, add a random word
     while len(password) < number_of_characters:
-        password += random.choice(words)
+        password += random.choice(password_parts) # This will add a random word to the password (it will be bigger than number_of_characters)
+
+    # password is bigger than needed amount of characters, so remove last letter until it is the correct length
+    while len(password) > number_of_characters:
+        password = password[:-1]
+    
     return password
 
-def return_all_words():
-    word_list = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'imbe', 'jackfruit', 'kiwi', 'lime', 'mango', 'nectarine', 'orange', 'papaya', 'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli', 'vanilla', 'watermelon', 'xigua', 'yuzu', 'zucchini']
+def list_of_words():
+    word_list = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape', 'Honeydew', 'Imbe', 'Jackfruit', 'Kiwi', 'Lime', 'Mango', 'Nectarine', 'Orange', 'Papaya', 'Quince', 'Raspberry', 'Strawberry', 'Tangerine', 'Ugli', 'Vanilla', 'Watermelon', 'Xigua', 'Yuzu', 'Zucchini']
     return word_list
+
+def list_of_characters():
+    character_list = list(string.ascii_letters)
+    return character_list
+
+def list_of_numbers():
+    number_list = list(string.digits)
+    return number_list
+
+def list_of_symbols():
+    symbol_list = list(string.punctuation)
+    return symbol_list
